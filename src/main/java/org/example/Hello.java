f8728c615669879064454885a05617095584a275
cwl <2785075155@qq.com>
2022-11-13 09:50:57 +0800

    第一次提交到dev分支

--- src/main/java/org/example/Hello.java
+++ src/main/java/org/example/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("第一次提交！");
+        System.out.println("第二次提交！");
+        System.out.println("第二次提交！");
     }
 }
